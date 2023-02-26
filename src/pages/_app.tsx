9ddee3eaf4c2d874cafc1a13942180aa76002c8f
HayHay <dev@hayhay.dev>
2023-02-26 03:21:37 -0500

    Colour changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,17 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import NavBar from "./components/NavBar";
+import { useRouter } from "next/router";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
   return (
     <SessionProvider session={session}>
-      <div className="bg-gradient-to-b from-primary to-accent">
-        <NavBar />
+      <div className="bg-neutral">
+        {router.pathname !== "/" && <NavBar />}
         <Component {...pageProps} />
       </div>
     </SessionProvider>
