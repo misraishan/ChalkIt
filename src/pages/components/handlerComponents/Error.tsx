b7737b7ae2c4dcc4420d1132154bbd5c165fc0b2
HayHay <dev@hayhay.dev>
2023-03-01 00:38:06 -0500

    Small components for error/loading

---
+++ src/pages/components/handlerComponents/Error.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h2 className="text-3xl font-bold">Something went wrong :(</h2>
+        </div>
+    );
+}
